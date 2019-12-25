6473d6a90972544a753a0520a821cea453d03c8a
Sriharsh Aditya <aditya.reus11@gmail.com>
2019-12-25 22:46:40 +0530

    trying to geolocate via ip

--- routes/getUserIp.js
+++ routes/getUserIp.js
@@ -1,17 +1,26 @@
 const router = require('express').Router();
 const os = require('os');
 const geoip = require('geoip-lite');
+const expressip = require('express-ip');
 var User = require('../models/user.js');
 var ifaces = os.networkInterfaces();
 
-router.get('/ip',(req,res)=>{
-    res.render('getUserIp.ejs');
-});
+router.use(expressip().getIpInfoMiddleware);
 
-router.post('/ip',(req,res)=>{
-    var clientIP = req.connection.remoteAddress || req.headers['x-forwarded-for'];
-    res.send(clientIP);
-});
+router.get('/', function (req, res) {
+    const ipInfo = req.ipInfo;
+    var message = `Hey, you are browsing from ${ipInfo.city}, ${ipInfo.country}`;
+    res.send(message);
+  });
+
+// router.get('/ip',(req,res)=>{
+//     res.render('getUserIp.ejs');
+// });
+
+// router.post('/ip',(req,res)=>{
+//     var clientIP = req.connection.remoteAddress || req.headers['x-forwarded-for'];
+//     res.send(clientIP);
+// });
 
 
 module.exports = router;
