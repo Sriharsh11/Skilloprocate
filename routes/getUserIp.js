5105b6d295d2594f312f919ef985af2bf78ef95c
Sriharsh Aditya <aditya.reus11@gmail.com>
2019-12-27 19:16:06 +0530

    location wise searching

--- routes/getUserIp.js
+++ routes/getUserIp.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const os = require('os');
 const geoip = require('geoip-lite');
 const expressip = require('express-ip');
+const geolib = require('geolib');
 var User = require('../models/user.js');
 var ifaces = os.networkInterfaces();
 
@@ -29,5 +30,36 @@ router.get('/ip', function (req, res) {
     });
   });
 
+  router.get('/nearestUsers',(req,res)=>{
+    var username = req.cookies['username'];
+    var closestUsers = [];
+    User.findOne({username: username},(err,user)=>{
+        if(err)
+        throw err;
+        else{
+            var lat = parseFloat(user.latitude);
+            var lng = parseFloat(user.longitude);
+            User.find({},(err,nearUsers)=>{
+                if(err)
+                throw err;
+                else{
+                    for(var i=0;i<nearUsers.length;i++){
+                        if(nearUsers[i].username!=username){
+                            if(geolib.isPointWithinRadius(
+                                { latitude: parseFloat(nearUsers[i].latitude), longitude: parseFloat(nearUsers[i].longitude) },
+                                { latitude: lat, longitude: lng },
+                                1000000
+                            )){
+                                closestUsers.push(nearUsers[i]);
+                            }
+                        }
+                    }
+                    res.render('nearestUsers.ejs',{closestUsers});
+                }
+            });
+        }
+    });
+  });
+  
 
 module.exports = router;
